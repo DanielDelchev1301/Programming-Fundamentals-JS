47195b7f47db4c3b7b339525beb6650d20770ebb
Daniel <danieldelchev1301@abv.bg>
2022-04-05 07:25:02 +0300

    Learn about arrays.

---
+++ arrays/reverseAnArrayOfNumbers.js
@@ -0,0 +1,12 @@
+function reverseAnArrayOfNumbers(num, array) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(array[i]);
+    }
+    let output = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output += `${arr[i]} `;
+    }
+    console.log(output);
+}
+reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
