f276a1d8c6dd5129c790401c4fedeea65d89f5a2
Daniel <danieldelchev1301@abv.bg>
2022-04-06 20:59:53 +0300

    Learn about arrays.

---
+++ arrays/sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(array) {
+    let first = array[0];
+    let last = array[array.length - 1];
+    console.log(first + last);
+    //console.log(array[0] + array[array.length - 1]);
+}
+sumFirstAndLastArrayElements([20, 30, 40]);
