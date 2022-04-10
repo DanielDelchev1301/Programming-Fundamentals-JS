df7d98e8c1801e7b731ea91cf291166b3fd77e48
Daniel <danieldelchev1301@abv.bg>
2022-04-10 08:36:09 +0300

    functions.

---
+++ functions/nxnMatrix.js
@@ -0,0 +1,11 @@
+function nxnMatrix(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            arr.push(num);
+        }
+        console.log(arr.join(' '));
+        arr = [];
+    }
+}
+nxnMatrix(3);
