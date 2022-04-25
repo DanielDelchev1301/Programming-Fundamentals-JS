44c6f61b817d74ad1908c2093eb4a82e9a7ad4fd
Daniel <danieldelchev1301@abv.bg>
2022-04-25 08:30:18 +0300

    learn

---
+++ objectAndClasses/city.js
@@ -0,0 +1,14 @@
+function city(obj) {
+    let entries = Object.entries(obj);
+    for (let [key, value] of entries) {
+        console.log(`${key} -> ${value}`);
+    }
+}
+city({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+}
+);
