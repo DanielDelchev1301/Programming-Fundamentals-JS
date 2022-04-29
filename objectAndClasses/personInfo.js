6663fd6cd4e85d55878c29255397cf1381cb8492
Daniel <danieldelchev1301@abv.bg>
2022-04-29 08:36:23 +0300

    learn

---
+++ objectAndClasses/personInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+    let obj = {};
+    obj.firstName = firstName;
+    obj.lastName = lastName;
+    obj.age = age;
+    return obj;
+}
+personInfo("George", 
+"Smith",
+"18");
